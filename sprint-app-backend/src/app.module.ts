d01bb41160cf5dd9d723d7d0572a483f0e3e2ba0
dorijanp <dorijanpiskovic1234@gmail.com>
2023-10-20 19:41:50 +0200

    remove unused service, fixed prisma module import

--- sprint-app-backend/src/app.module.ts
+++ sprint-app-backend/src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [PrismaModule, UserModule],
   controllers: [],
-  providers: [AppService],
+  providers: [PrismaService],
 })
 export class AppModule {}
