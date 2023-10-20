4ca9f092f7735757acdcf7076c56335ba80ce8bb
dorijanp <dorijanpiskovic1234@gmail.com>
2023-10-20 19:45:18 +0200

    create task module, service & controller

--- sprint-app-backend/src/app.module.ts
+++ sprint-app-backend/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { PrismaService } from './prisma/prisma.service';
+import { SprintModule } from './sprint/sprint.module';
+import { TaskModule } from './task/task.module';
 
 @Module({
-  imports: [PrismaModule, UserModule],
+  imports: [PrismaModule, UserModule, SprintModule, TaskModule],
   controllers: [],
   providers: [PrismaService],
 })
