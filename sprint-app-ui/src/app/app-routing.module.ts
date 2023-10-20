c3a9e199784bcc72cc6c1bc4c68bdaf50b9e580e
dorijanp <dorijanpiskovic1234@gmail.com>
2023-10-20 22:41:33 +0200

    create dashboard

--- sprint-app-ui/src/app/app-routing.module.ts
+++ sprint-app-ui/src/app/app-routing.module.ts
@@ -7,6 +7,11 @@ const routes: Routes = [
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
   },
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./dashboard/dashboard.module').then((m) => m.DashboardModule),
+  },
 ];
 
 @NgModule({
---
+++ sprint-app-ui/src/app/shared/models/user.ts
@@ -0,0 +1,7 @@
+export interface User {
+  id: number;
+  email: string;
+  hash: string;
+  firstName: string;
+  lastName: string;
+}
