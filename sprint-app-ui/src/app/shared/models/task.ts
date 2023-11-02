1ace03227cfe3f96cab3533376535e6a97f691c7
dorijanp <dorijanpiskovic1234@gmail.com>
2023-11-02 22:51:21 +0100

    update models

--- sprint-app-ui/src/app/shared/models/task.ts
+++ sprint-app-ui/src/app/shared/models/task.ts
@@ -1,3 +1,11 @@
+import { User } from './user';
+
 export interface Task {
+  id: number;
+  createdAt: Date;
+  description: string;
+  status: string;
   title: string;
+  assignee: User;
+  creator: User;
 }
--- sprint-app-ui/src/app/shared/models/user.ts
+++ sprint-app-ui/src/app/shared/models/user.ts
@@ -4,4 +4,5 @@ export interface User {
   hash: string;
   firstName: string;
   lastName: string;
+  name?: string;
 }
